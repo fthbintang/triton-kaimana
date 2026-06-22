3e53e51474d5c1f2f4798e30ad614e40b1d9a859
fthbintang <bintangfathehah@gmail.com>
2026-06-22 11:02:05 +0900

    feat(database): buat tabel surat_kuasa, pemberi_kuasa, dan penerima_kuasa

---
+++ database/migrations/2026_06_22_011236_create_surat_kuasa_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surat_kuasa', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_hp_pemohon'); // Kontak utama pengisi formulir
+            $table->timestamps(); // Mencatat pendaftar terbaru secara otomatis
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surat_kuasa');
+    }
+};
